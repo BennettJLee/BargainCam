9c0d0ea038aec282d6540b0333dc90c87850f763
Dylan Nunn <dylann1342@gmail.com>
2023-10-11 21:21:49 +1300

    basic text recognition functionality (no filter currently on what text to find and reads all it can see)

--- app/src/main/java/com/example/bargaincam/MainActivity.kt
+++ app/src/main/java/com/example/bargaincam/MainActivity.kt
@@ -1,29 +1,69 @@
-package com.example.bargaincamprivate
+package com.example.bargaincam
 
 import android.R
 import android.R.attr.bitmap
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.media.Image
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.PopupWindow
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.OptIn
+import androidx.camera.core.AspectRatio
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import com.example.bargaincamprivate.databinding.ActivityMainBinding
+import androidx.lifecycle.LifecycleOwner
+import com.example.bargaincam.databinding.ActivityMainBinding
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 
 class MainActivity : ComponentActivity() {
@@ -51,7 +91,9 @@ class MainActivity : ComponentActivity() {
             activityResultLauncher.launch(REQUIRED_PERMISSIONS.toTypedArray())
         } else {
             // if so, display the camera preview to the screen
-            startCamera()
+            setContent {
+                CameraLaunch()
+            }
 
 
             // ** TESTING: testing the pop-up **
@@ -71,16 +113,109 @@ class MainActivity : ComponentActivity() {
     /**
      * This function starts the Camera Preview
      */
-    private fun startCamera() {
-        val previewView: PreviewView = viewBinding.viewFinder
-        cameraController = LifecycleCameraController(baseContext)
-        cameraController.bindToLifecycle(this)
-        cameraController.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+//    private fun startCamera() {
+//        val previewView: PreviewView = viewBinding.viewFinder
+//        cameraController = LifecycleCameraController(baseContext)
+//        cameraController.bindToLifecycle(this)
+//        cameraController.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+//        previewView.controller = cameraController
+//    }
+
+    @Composable
+    private fun CameraLaunch() {
+
+        val context: Context = LocalContext.current
+        val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+        val cameraController: LifecycleCameraController = remember { LifecycleCameraController(context) }
+        var detectedText: String by remember { mutableStateOf(" ") }
+        var NumText: String
+
+        fun onTextUpdated(updatedText: String) {
+            NumText = updatedText.filter { it.isDigit() }
+            detectedText = NumText
+            promotionWindow.closePromotionWindow()
+            promotionWindow.updateAisle(detectedText)
+            promotionWindow.showPromotionWindow()
+        }
+
+        Scaffold(modifier = Modifier.fillMaxSize())
+        { paddingValues: PaddingValues ->
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter)
+            {
+
+                AndroidView(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    factory = { context ->
+                        PreviewView(context).apply {
+                            layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+                            setBackgroundColor(Color.BLACK)
+                            implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                            scaleType = PreviewView.ScaleType.FILL_START
+                        }.also { previewView ->
+                            startTextRecognition(
+                                context = context,
+                                cameraController = cameraController,
+                                lifecycleOwner = lifecycleOwner,
+                                previewView = previewView,
+                                onDetectedTextUpdated = ::onTextUpdated
+                            )
+                        }
+                    }
+                )
+            }
+        }
+    }
+
+    private fun startTextRecognition(context: Context, cameraController: LifecycleCameraController, lifecycleOwner: LifecycleOwner,
+        previewView: PreviewView, onDetectedTextUpdated: (String) -> Unit) {
+        cameraController.imageAnalysisTargetSize = CameraController.OutputSize(AspectRatio.RATIO_16_9)
+        cameraController.setImageAnalysisAnalyzer(ContextCompat.getMainExecutor(context), TextRecognitionAnalyzer(onDetectedTextUpdated = onDetectedTextUpdated))
+        cameraController.bindToLifecycle(lifecycleOwner)
         previewView.controller = cameraController
     }
 
+    class TextRecognitionAnalyzer(private val onDetectedTextUpdated: (String) -> Unit) : ImageAnalysis.Analyzer {
+
+        companion object {
+            const val THROTTLE_TIMEOUT_MS = 1_000L
+        }
+
+        private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+        private val textRecognizer: TextRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        @OptIn(ExperimentalGetImage::class)
+        override fun analyze(imageProxy: ImageProxy) {
+            scope.launch {
+                val mediaImage: Image = imageProxy.image ?: run { imageProxy.close(); return@launch }
+                val inputImage: InputImage = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+                suspendCoroutine { continuation ->
+                    textRecognizer.process(inputImage)
+                        .addOnSuccessListener { visionText: Text ->
+                            val detectedText: String = visionText.text
+                            if (detectedText.isNotBlank()) {
+                                onDetectedTextUpdated(detectedText)
+                            }
+                        }
+                        .addOnCompleteListener {
+                            continuation.resume(Unit)
+                        }
+                }
+
+                delay(THROTTLE_TIMEOUT_MS)
+            }.invokeOnCompletion { exception ->
+                exception?.printStackTrace()
+                imageProxy.close()
+            }
+        }
+    }
+
+
+
     /**
-     * This function asks the user for there permissions
+     * This function asks the user for their permissions
      */
     private val activityResultLauncher =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
@@ -96,7 +231,9 @@ class MainActivity : ComponentActivity() {
                 Toast.makeText(this, "Both Camera and Location permissions are needed to use this feature.", Toast.LENGTH_LONG).show()
             } else {
                 // If so, display the camera preview
-                startCamera()
+                setContent {
+                    CameraLaunch()
+                }
 
 
                 // ** TESTING: testing the pop-up **
